6abeb7fcdce0b4e57467661747f91ab39c48f183
lucas <lucasmacielaguiar@gmail.com>
2019-08-21 11:29:04 -0300

    add paint unique block

--- floodFill/main.py
+++ floodFill/main.py
@@ -1,8 +1,12 @@
 import pygame
 import os
+import random
 
+red = (255, 0, 0)
 green = (0, 255, 0)
 black = (0, 0, 0)
+colors = [red, green, black]
+
 running = True
 posX = 10
 posY = 20
@@ -20,14 +24,37 @@ class Board(object):
         self.make_board()
 
     def make_board(self):
-        for i in range(10):
-            for j in range(10):
-                self.board.append(pygame.rect.Rect(
-                    (j * 20 + 5, i * 20 + 5, 20, 20)))
+        size_board = 20
+
+        for i in range(size_board):
+            for j in range(size_board):
+                block = type('', (), {})()
+                block.body = pygame.rect.Rect((j * 20 + 5, i * 20 + 5, 20, 20))
+                block.color = colors[random.randint(0, len(colors) - 1)]
+                self.board.append(block)
 
     def draw(self):
+        if pygame.mouse.get_pressed()[0]:
+            # if left button is clicked
+            position = pygame.mouse.get_pos()
+            print(position)
+            self.selectBlock(position)
         for block in self.board:
-            pygame.draw.rect(screen, black, block)
+            pygame.draw.rect(screen, block.color, block.body)
+
+    def selectBlock(self, position):
+        psx = position[0]
+        psy = position[1]
+        for block in self.board:
+            if (psx > block.body.left and psx < block.body.right and psy > block.body.top and psy < block.body.bottom):
+                self.paint(block)
+                break
+
+    def paint(self, block):
+        '''
+            Paint similar color blocks like a flood fill graph
+        '''
+        block.color = black
 
 
 def main():
